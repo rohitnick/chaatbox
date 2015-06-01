e26f03002e41a70a11a146e4128e2926b91ede10
rajat <agarwalrajat088@gmail.com>
2015-06-01 19:59:33 +0530

    cart modified

---
+++ app/assets/javascripts/plus.js
@@ -0,0 +1,80 @@
+function plus1()
+{
+var x=$('#num1').val();
+$('#num1').val(x-(-1));
+var y=$('#prices1').html();
+$('#pricess1').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus2()
+{
+var x=$('#num2').val();
+$('#num2').val(x-(-1));
+var y=$('#prices2').html();
+$('#pricess2').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus3()
+{
+var x=$('#num3').val();
+$('#num3').val(x-(-1));
+var y=$('#prices3').html();
+$('#pricess3').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus4()
+{
+var x=$('#num4').val();
+$('#num4').val(x-(-1));
+var y=$('#prices4').html();
+$('#pricess4').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus5()
+{
+var x=$('#num5').val();
+$('#num5').val(x-(-1));
+var y=$('#prices5').html();
+$('#pricess5').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus6()
+{
+var x=$('#num6').val();
+$('#num6').val(x-(-1));
+var y=$('#prices6').html();
+$('#pricess6').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus7()
+{
+var x=$('#num7').val();
+$('#num7').val(x-(-1));
+var y=$('#prices7').html();
+$('#pricess7').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
+function plus8()
+{
+var x=$('#num8').val();
+$('#num8').val(x-(-1));
+var y=$('#prices8').html();
+$('#pricess8').html((x-(-1))*y);
+var total_order=$('#total').html();
+$('#total').html(total_order-(-y));
+}
+
