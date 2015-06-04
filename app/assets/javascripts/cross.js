ab333aeb22036f3055ee2e6c885ab68b76265fe3
rajat <agarwalrajat088@gmail.com>
2015-06-04 21:47:20 +0530

    cxv

---
+++ app/assets/javascripts/cross.js
@@ -0,0 +1,69 @@
+//$(document).ready(function(){
+//$('.line-item').each(function(i)
+//{
+//(this).find
+function cross1()
+{ 
+var x=$('#num1').val();
+var y=$('#prices1').html();
+$('#line-items #tag1').remove();
+$('#1').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross2()
+{ 
+var x=$('#num2').val();
+var y=$('#prices2').html();
+$('#line-items #tag2').remove();
+$('#2').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross3()
+{ 
+var x=$('#num3').val();
+var y=$('#prices3').html();
+$('#line-items #tag3').remove();
+$('#3').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross4()
+{ 
+var x=$('#num4').val();
+var y=$('#prices4').html();
+$('#line-items #tag4').remove();
+$('#4').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross5()
+{ 
+var x=$('#num5').val();
+var y=$('#prices5').html();
+$('#line-items #tag5').remove();
+$('#5').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross6()
+{ 
+var x=$('#num6').val();
+var y=$('#prices6').html();
+$('#line-items #tag6').remove();
+$('#6').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross7()
+{ 
+var x=$('#num7').val();
+var y=$('#prices7').html();
+$('#line-items #tag7').remove();
+$('#7').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+function cross8()
+{ 
+var x=$('#num8').val();
+var y=$('#prices8').html();
+$('#line-items #tag8').remove();
+$('#8').removeClass('js-product-selected');
+$('#total').html($('#total').html()-(x*y));
+}
+
