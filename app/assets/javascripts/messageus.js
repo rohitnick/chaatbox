b39b989347c8b46b935a92226e44e321b506ccfc
rajat <agarwalrajat088@gmail.com>
2015-06-07 00:02:22 +0530

    gh

--- app/assets/javascripts/messageus.js
+++ app/assets/javascripts/messageus.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $('form').submit(function(e){
 e.preventDefault();
-$("#submit").attr('data-dismiss','modal');
-//alert("ghgg");
+var n=$("#name").val();
+alert(n);
 });
 });
