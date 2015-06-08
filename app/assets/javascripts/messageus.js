c99fd4d67b66d0cd92a19e6abad1907f51f5aeab
rajat <agarwalrajat088@gmail.com>
2015-06-08 16:21:12 +0530

    uu

--- app/assets/javascripts/messageus.js
+++ app/assets/javascripts/messageus.js
@@ -6,12 +6,12 @@ var t=$('#total').html();
 var m=document.getElementById("mobile").value;
 var e=document.getElementById("email").value;
 var a=document.getElementById("message").value;
-alert(a+m+e+n);
 $.ajax({
    type: "POST",
    url: 'message',
    dataType: 'json',
    data: $.param({contacts: {name: n,email: e,mobile: m,message: a}})
 });
+$("#close").click();
 });
 });
