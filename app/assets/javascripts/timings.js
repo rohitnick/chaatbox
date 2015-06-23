b1621da9b8b9622d656323b736a530cc6975111e
rajat <agarwalrajat088@gmail.com>
2015-06-23 20:03:12 +0530

    dsfedcxdf

--- app/assets/javascripts/timings.js
+++ app/assets/javascripts/timings.js
@@ -3,9 +3,9 @@ function timings()
 var date=new Date();
 var d=date.getHours();
 
-if(d<11 || d>21)
+if(d<11 || d>19)
 {
 
-$("#time").html("<b>Note:</b>Though we will recieve your order, we <br> deliver from 11 am to 10 pm. <br>We will call you as soon as we open again.");
+$("#time").html("<b>Note:</b>Though we will recieve your order, we <br> deliver from 11 am to 8 pm. <br>We will call you as soon as we open again.");
 }
 }
