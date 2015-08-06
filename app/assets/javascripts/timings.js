8516345b04a50bdf8c687fcd9f50a2f071400a84
rajat <agarwalrajat088@gmail.com>
2015-08-06 08:51:21 +0530

    12 pm

--- app/assets/javascripts/timings.js
+++ app/assets/javascripts/timings.js
@@ -5,6 +5,6 @@ var d=date.getHours();
 
 if(d<12 || d>21)
 {
-$("#time").html("<b>Note: </b> Though we will recieve your order, we deliver from 12 am to 10 pm. <br>We will call you as soon as we open.");
+$("#time").html("<b>Note: </b> Though we will recieve your order, we deliver from 12 pm to 10 pm. <br>We will call you as soon as we open.");
 }
 }
