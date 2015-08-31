55487b3b52cb73925f0d06ee71245a0179c1ba23
rajat <agarwalrajat088@gmail.com>
2015-08-31 21:57:07 +0530

    11pmtoday

--- app/assets/javascripts/timings.js
+++ app/assets/javascripts/timings.js
@@ -3,8 +3,8 @@ function timings()
 var date=new Date();
 var d=date.getHours();
 
-if(d<15 || d>21)
+if(d<11 || d>21)
 {
-$("#time").html("<b>Note: </b> Though we will recieve your order, we will deliver today from 3 pm to 10 pm. <br>We will call you as soon as we open.");
+$("#time").html("<b>Note: </b> Though we will recieve your order, we deliver from 11 am to 10 pm. <br>We will call you as soon as we open.");
 }
 }
