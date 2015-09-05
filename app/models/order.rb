5570e152f3756e974a0f173ee2c528c2c9965880
rajat <agarwalrajat088@gmail.com>
2015-09-05 22:40:37 +0530

    source

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,6 @@ class Order < ActiveRecord::Base
   has_many :products, :through => :line_items
 
   # Do not change the order of values nor include any new value in between, always add new values in the end.
-  enum status: [:Arrived, :"In Progress", :Completed, :Cancelled, :Fake, :"Not Deliverable",:Tinyowl ,:FoodPanda]
+  enum status: [:Arrived, :"In Progress", :Completed, :Cancelled, :Fake, :"Not Deliverable"]
+  enum source: [:Call ,:Whatsapp ,:Tinyowl ,:Foodpanda ,:Website ,:other]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809094523) do
+ActiveRecord::Schema.define(version: 20150905170707) do
 
   create_table "leavemessages", force: :cascade do |t|
     t.string   "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150809094523) do
     t.string   "payableamount"
     t.integer  "status"
     t.string   "selectedtime"
+    t.integer  "source"
   end
 
   create_table "products", force: :cascade do |t|
