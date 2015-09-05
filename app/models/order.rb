2165bd8c553c9e81e242770c39a900b03b36b999
rajat <agarwalrajat088@gmail.com>
2015-09-05 22:21:14 +0530

    changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ActiveRecord::Base
   has_many :products, :through => :line_items
 
   # Do not change the order of values nor include any new value in between, always add new values in the end.
-  enum status: [:Arrived, :"In Progress", :Completed, :Cancelled, :Fake, :"Not Deliverable"]
+  enum status: [:Arrived, :"In Progress", :Completed, :Cancelled, :Fake, :"Not Deliverable",:Tinyowl,:FoodPanda]
 end
