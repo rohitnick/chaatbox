9db16f5947dfa251b4aae0974330697c8af50368
rajat <agarwalrajat088@gmail.com>
2015-12-29 11:08:35 +0530

    changes

--- app/assets/javascripts/timings.js
+++ app/assets/javascripts/timings.js
@@ -2,7 +2,7 @@ function timings()
 {
 var date=new Date();
 var d=date.getHours();
-if(d<13 || (d>21)
+if(d<13 || d>21)
 {
 $("#time").html("<b>Note: </b> Though we will recieve your order, we deliver from 2 pm to 10 pm. <br>We will call you as soon as we open.");
 }
